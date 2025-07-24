265aa59a88307ba78ed1486a1d38e16a63fe3c94
LEOMAZENDA2 <leonildo.mazenda@hotmail.com>
2025-07-24 23:27:54 +0100

    implementing getProduct by Id

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -3,5 +3,5 @@ global using Mapster;
 global using MediatR;
 global using Marten;
 global using Catalog.API.Models;
-global using BuildingBlocks.CQRS.Commands;
-
+global using BuildingBlocks.CQRS.Queries;
+global using Catalog.API.Exceptions;
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductVyId/GetProductByIdHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.GetProductVyId;
+
+internal class GetProductByIdQueryHandler
+    (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+    : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+        var product = session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (product is null)
+        {
+            logger.LogWarning("Product with Id {Id} not found", query.Id);
+            throw new ProductNotFoundException();
+        }
+
+    }
+}
+
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product Product);
